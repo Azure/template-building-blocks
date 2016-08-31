24e93590aeff67469652aa57f2590aa8c609cd70
RohitSharma-pnp <roshar@microsoft.com>
2016-08-30 18:30:05 -0700

    updated script for enabling routes

--- templates/resources/Microsoft.Compute/virtualMachines/extensions/linux/enable-iptables-routes/enable-iptables-routes.sh
+++ templates/resources/Microsoft.Compute/virtualMachines/extensions/linux/enable-iptables-routes/enable-iptables-routes.sh
@@ -2,18 +2,23 @@
 bash -c "echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf"
 sysctl -p /etc/sysctl.conf
 
-PRIVATE_IP_ADDRESS=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-PUBLIC_IP_ADDRESS=$(wget http://ipinfo.io/ip -qO -)
+LB_TYPE = $1
+DEST_IP_ADDRESS = $2
+if [[ "$LB_TYPE" == "public" ]] 
+then
+	IP_ADDRESS=$(wget http://ipinfo.io/ip -qO -)
+else
+	IP_ADDRESS=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+fi
 
 iptables -F
 iptables -t nat -F
 iptables -X
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 10.0.1.254:80
-iptables -t nat -A POSTROUTING -p tcp -d 10.0.1.254 --dport 80 -j SNAT --to-source $PRIVATE_IP_ADDRESS
-iptables -t nat -A POSTROUTING -p tcp -d 10.0.1.254 --dport 80 -j SNAT --to-source $PUBLIC_IP_ADDRESS
-iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination 10.0.1.254:443
-iptables -t nat -A POSTROUTING -p tcp -d 10.0.1.254 --dport 443 -j SNAT --to-source $PRIVATE_IP_ADDRESS
-iptables -t nat -A POSTROUTING -p tcp -d 10.0.1.254 --dport 443 -j SNAT --to-source $PUBLIC_IP_ADDRESS
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination "${DEST_IP_ADDRESS}:80"
+iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination "${DEST_IP_ADDRESS}:443"
+iptables -t nat -A POSTROUTING -p tcp -d $DEST_IP_ADDRESS --dport 80 -j SNAT --to-source $IP_ADDRESS
+iptables -t nat -A POSTROUTING -p tcp -d $DEST_IP_ADDRESS --dport 80 -j SNAT --to-source $IP_ADDRESS
+
 service ufw stop
 service ufw start
 
